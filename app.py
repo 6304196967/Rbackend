47c92de28fe9d988c5a4f3a2a57f905b947c56f3
sainath <bhumireddyrajareddy89@gmail.com>
2025-03-06 16:39:15 +0530

    hijth

--- app.py
+++ app.py
@@ -2,6 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+try:
+    with open( "./columns.json", "r") as f:
+        data_columns = json.load(f)["data_columns"]
+        locations = data_columns[3:]
+except (FileNotFoundError, json.JSONDecodeError):
+    data_columns, locations = [], []
+
+try:
+    with open( "./YTproject.pickle", "rb") as f:
+        model = pickle.load(f)
+except FileNotFoundError:
+    model = None
+
 @app.route('/')
 def index():
     return render_template('index.html')
